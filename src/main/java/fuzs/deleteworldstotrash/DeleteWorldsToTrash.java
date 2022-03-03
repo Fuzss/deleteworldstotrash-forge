9e86c1929b44a198504fcd2a40567d599966d37d
Fuzss <28218241+Fuzss@users.noreply.github.com>
2022-03-03 22:57:16 +0100

    compiled for 1.18.2

--- src/main/java/fuzs/deleteworldstotrash/DeleteWorldsToTrash.java
+++ src/main/java/fuzs/deleteworldstotrash/DeleteWorldsToTrash.java
@@ -1,13 +1,13 @@
 package fuzs.deleteworldstotrash;
 
 import net.minecraftforge.fml.common.Mod;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Mod(DeleteWorldsToTrash.MOD_ID)
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DeleteWorldsToTrash {
     public static final String MOD_ID = "deleteworldstotrash";
     public static final String MOD_NAME = "Delete Worlds To Trash";
-    public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 }
